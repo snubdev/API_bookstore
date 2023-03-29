6231596ee9a180ab96157b6f8ffd41e1a23513c6
Maicon Leandro <snubhousedev@gmail.com>
2023-03-29 00:12:09 -0300

    Revisando o código

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2smbq)gmo0aos7u@p95t*749c&^)&m=6e0vfcegzwjpo8h=tva'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
