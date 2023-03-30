8d52e021cff121e0e9554b1b70c5031055cb68b3
Maicon Leandro <snubhousedev@gmail.com>
2023-03-30 10:13:08 -0300

    Revisando o código

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2smbq)gmo0aos7u@p95t*749c&^)&m=6e0vfcegzwjpo8h=tva'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
